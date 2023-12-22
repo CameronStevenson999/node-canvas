e89db3b0ab6003bce1886e5a830436af7f20d211
cameron.stevenson <cameron.stevenson@groundprobe.com>
2023-12-22 11:09:31 +1000

    Nodes can be added to the board

--- src/components/BoardComponent/index.tsx
+++ src/components/BoardComponent/index.tsx
@@ -1,12 +1,23 @@
-import { Component, createSignal, onMount } from "solid-js";
+import { Accessor, Component, For, Setter, createSignal, onMount } from "solid-js";
 import styles from "./styles.module.css";
 import ButtonsComponent from "../ButtonsComponent";
+import NodeComponent from "../NodeComponent";
+
+interface Node {
+    id: string;
+    numberInputs: number;
+    numberOutputs: number;
+    prevPosition: { get: Accessor<{ x: number; y: number }>; set: Setter<{ x: number; y: number }> };
+    currPosition: { get: Accessor<{ x: number; y: number }>; set: Setter<{ x: number; y: number }> };
+}
 
 const BoardComponent: Component = () => {
     const [grabbingBoard, setGrabbingBoard] = createSignal<boolean>(false);
     const [scale, setScale] = createSignal<number>(1);
     const [clickedPosition, setClickedPosition] = createSignal<{ x: number; y: number }>({ x: -1, y: -1 });
     const [selectedNode, setSelectedNode] = createSignal<string | null>(null);
+
+    const [nodes, setNodes] = createSignal<Node[]>([]);
 
     onMount(() => {
         const boardElement = document.getElementById("board");
@@ -59,9 +70,34 @@ const BoardComponent: Component = () => {
         }
     }
 
-    function handleOnClickAdd(numberInputs: number, numberOutputs: number) {}
+    function handleOnClickAdd(numberInputs: number, numberOutputs: number) {
+        const randomX = Math.random() * window.innerWidth;
+        const randomY = Math.random() * window.innerHeight;
+
+        const [nodePrev, setNodePrev] = createSignal<{ x: number; y: number }>({ x: randomX, y: randomY });
+        const [nodeCurr, setNodeCurr] = createSignal<{ x: number; y: number }>({ x: randomX, y: randomY });
+
+        setNodes([
+            ...nodes(),
+            {
+                id: `node_${Math.random().toString(36).substring(2, 8)}`,
+                numberInputs: numberInputs,
+                numberOutputs: numberOutputs,
+                prevPosition: { get: nodePrev, set: setNodePrev },
+                currPosition: { get: nodeCurr, set: setNodeCurr },
+            },
+        ]);
+    }
 
     function handleOnClickDelete() {}
+
+    function handleOnMouseDownNode(id: string, event: any) {}
+
+    function handleOnMouseDownOutput(outputPositionX: number, outputPositionY: number, nodeId: string, outputIndex: number) {}
+
+    function handleOnMouseEnterInput(inputPositionX: number, inputPositionY: number, nodeId: string, inputIndex: number) {}
+
+    function handleOnMouseLeaveInput(nodeId: string, inputIndex: number) {}
 
     return (
         <div id="boardWrapper" class={styles.wrapper}>
@@ -72,7 +108,25 @@ const BoardComponent: Component = () => {
                 onMouseDown={handleOnMouseDownBoard}
                 onMouseUp={handleOnMouseUpBoard}
                 onMouseMove={handleOnMouseMove}
-            ></div>
+            >
+                <For each={nodes()}>
+                    {(node: Node) => (
+                        <NodeComponent 
+                            id={node.id}
+                            x={node.currPosition.get().x}
+                            y={node.currPosition.get().y}
+                            numberInputs={node.numberInputs}
+                            numberOutputs={node.numberOutputs}
+                            selected={selectedNode() === node.id}
+                            onMouseDownNode={handleOnMouseDownNode}
+                            onMouseDownOutput={handleOnMouseDownOutput}
+                            onMouseEnterInput={handleOnMouseEnterInput}
+                            onMouseLeaveInput={handleOnMouseLeaveInput}
+                        />
+                    )}
+                </For>
+            </div>
+
         </div>
     );
 };
